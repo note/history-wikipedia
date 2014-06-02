6df155e0363a00176533e92cfe9e2aa6ce7055d6
Michał Sitko <msitko68@gmail.com>
2014-06-02 19:59:56 +0200

    Fix

--- graph/build_graph.sh
+++ graph/build_graph.sh
@@ -5,6 +5,8 @@ export DB_PASS=root
 export DB_HOST=127.0.0.1
 export DB=wikipedia
 
+cd "$(dirname "$0")"
+
 echo "Creating additional tables..."
 mysql -u $DB_USER -h $DB_HOST $DB --password=$DB_PASS < additional_tables.sql
 echo "Created additional tables"
